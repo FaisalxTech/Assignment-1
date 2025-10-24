9ed6d15c653b80ab64aaef27eb647338d4bc9d63
Faisal Hussain <faisalthisside@gmail.com>
2025-10-24 23:26:29 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,26 @@
+# Take input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform calculations
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+# Display results
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
+
+
+# Taking user's first name and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating first name and last name to form full name
+full_name = first_name + " " + last_name
+
+# Printing a personalized greeting message
+print("Hello, " + full_name + "! Welcome to the python program!")
